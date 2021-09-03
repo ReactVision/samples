6c6523727a975afd7258258d2079349779d9549b
Kurt VanDusen <info@equationalapplications.com>
2021-09-03 10:18:22 -0400

    Rename app arReticle -b com.myviroapp.arreticle

--- ARReticle/ios/arReticleTests/arReticleTests.m
+++ ARReticle/ios/arReticleTests/arReticleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface myviroappTests : XCTestCase
+@interface arReticleTests : XCTestCase
 
 @end
 
-@implementation myviroappTests
+@implementation arReticleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
