e8243c094f1d71fa329cf0539c1004a45da64a04
Kurt VanDusen <info@equationalapplications.com>
2021-09-09 19:17:46 -0400

    Add SafeAreaView

--- ARDrivingCar/src/screens/ARScreen.js
+++ ARDrivingCar/src/screens/ARScreen.js
@@ -32,26 +32,26 @@ class ARScreen extends Component {
 
   constructor(props) {
     super(props)
-    
+
     this.state = {
       // initial state here
-      showInstructions : true,
-      instructionOpacity : new Animated.Value(1),
-      carControlsOpacity : new Animated.Value(0),
-      isReady : false,
-      isOverPlane : false,
-      left : false,
-      up : false,
-      right : false,
-      down : false,
-      touchLocation : "0,0",
-      leftRightRatio : 0,
-      shouldResetCar : false,
-      isRecording : false,
-      shouldPlayMedia : true, // whether or not the AR session should play media (probably because its hidden)
-      hours : '00',
-      minutes : '00',
-      seconds : '00',
+      showInstructions: true,
+      instructionOpacity: new Animated.Value(1),
+      carControlsOpacity: new Animated.Value(0),
+      isReady: false,
+      isOverPlane: false,
+      left: false,
+      up: false,
+      right: false,
+      down: false,
+      touchLocation: "0,0",
+      leftRightRatio: 0,
+      shouldResetCar: false,
+      isRecording: false,
+      shouldPlayMedia: true, // whether or not the AR session should play media (probably because its hidden)
+      hours: '00',
+      minutes: '00',
+      seconds: '00',
     }
 
     // bind functions here
@@ -92,336 +92,340 @@ class ARScreen extends Component {
   render() {
 
     let text = "" + (this.state.left ? "Left," : "") + (this.state.up ? "Up," : "")
-               + (this.state.right ? "Right," : "") + (this.state.down ? "Down," : "")
+      + (this.state.right ? "Right," : "") + (this.state.down ? "Down," : "")
 
     return (
-      <View style={styles.outerContainer}>
-        
-        {this.getViroARView()}
+      <SafeAreaView style={styles.outerContainer}>
+        <View style={styles.outerContainer}>
 
-        {this.getCarControls()}
+          {this.getViroARView()}
 
-        {/* Get instructions and ready */}
-        {this.getReadyUI()}
-        {this.getInstructions()}
+          {this.getCarControls()}
 
-      </View>
+          {/* Get instructions and ready */}
+          {this.getReadyUI()}
+          {this.getInstructions()}
+
+        </View>
+      </SafeAreaView>
     )
   }
 
   getViroARView() {
     // use viroAppProps to pass in "changing/dynamic" values, passProps is "not" dynamic.
     let viroAppProps = {
-      direction : (this.state.left ? 1 : 0) + (this.state.up ? 2 : 0) + (this.state.right ? 4 : 0) + (this.state.down ? 8 : 0),
-      leftRightRatio : this.state.leftRightRatio,
-      shouldResetCar : this.state.shouldResetCar,
-      isReady : this.state.isReady,
-      setIsOverPlane : this.setIsOverPlane,
+      direction: (this.state.left ? 1 : 0) + (this.state.up ? 2 : 0) + (this.state.right ? 4 : 0) + (this.state.down ? 8 : 0),
+      leftRightRatio: this.state.leftRightRatio,
+      shouldResetCar: this.state.shouldResetCar,
+      isReady: this.state.isReady,
+      setIsOverPlane: this.setIsOverPlane,
     }
-  return (
-    <ViroARSceneNavigator
-    ref={(ref)=>{this.arNavigator = ref}}
-    viroAppProps={viroAppProps}
-    initialScene={{
-      scene: ARScene,
-      passProps: {
-        onARInitialized: this.onARInitialized,
-        onPosterFound: this.onPosterFound,
-        onExperienceFinished : this.onExperienceFinished,
-        onARSceneCreated : this.onARSceneCreated,
-      },
-    }} />
-);
-}
-
-getCarControls() {
-
-return (
-  <Animated.View style={{position : 'absolute', width : '100%',
-                         height : '100%', opacity : this.state.carControlsOpacity}}>
-    {/* These are the controls to drive the car */}
-    {this.getDrivingPedals()}
-    {this.getResetButton()}
-    {this.getJoystick()}
-  </Animated.View>
-)
-}
-
-getDrivingPedals() {
-return (
-  <View style={styles.drivingButtonsContainer} >
-
-    <View style={styles.drivingButton} >
-      <Image style={styles.pedalImage} opacity={this.state.down ? 0 : 1}
-        source={require('../assets/pedal_reverse.png')} />
-      <Image style={styles.pedalImage} opacity={!this.state.down ? 0 : 1} 
-        source={require('../assets/pedal_reverse_press.png')}/>
-      <View style={styles.pedalTouchArea} onTouchStart={this.getPressDown('down')}
-      onTouchEnd={this.getPressUp('down')} />
-    </View>
-
-    <View style={styles.drivingButton} >
-      <Image style={styles.pedalImage} opacity={this.state.up ? 0 : 1}
-        source={require('../assets/pedal_accel.png')} />
-      <Image style={styles.pedalImage} opacity={!this.state.up ? 0 : 1} 
-        source={require('../assets/pedal_accel_press.png')}/>
-      <View style={styles.pedalTouchArea} onTouchStart={this.getPressDown('up')}
-      onTouchEnd={this.getPressUp('up')} />
-    </View>
-
-  </View>
-)
-}
-
-getJoystick() {
-let rotation = '' + (this.state.left ? '-' : '') + Math.round(this.state.leftRightRatio * 90) + 'deg'
-
-let joystickButtonStyle = {
-  height: 130,
-  width: 200,
-  resizeMode : 'contain',
-  transform: [{rotate : rotation}]
-}
-
-/*
-  This is the joystick/steering wheel component, since the image is rotating, we need
-  that "invisible" view to capture the touch events.
- */
-return (
-  <View style={styles.joystickContainer} >
-    <Image style={joystickButtonStyle} source={require('../assets/steering_wheel.png')}/>
-    <View style={styles.joystickTouchArea} onTouchStart={this.joystickStart} 
-    onTouchMove={this.joystickMove} onTouchEnd={this.joystickEnd} />
-  </View>
-)
-}
-
-getResetButton() {
-return (
-  <Pressable
-    style={styles.resetButton}
-    onPress={this.resetCar}
-    activeOpacity={0.6} >
-    <Image style={styles.resetImage} source={require('../assets/icon_refresh.png')}/>
-  </Pressable>
-)
-}
-
-resetCar() {
-this.setState({
-  shouldResetCar : true,
-})
-
-// reset the flag 1 second later.
-setTimeout(()=>{
-  this.setState({
-    shouldResetCar : false,
-  })
-}, 1000)
-}
-
-getPressDown(key) {
-return ()=>{
-  let dict = {}
-  dict[key] = true
-  this.setState(dict)
-}
-}
-
-getPressUp(key) {
-return ()=>{
-  let dict = {}
-  dict[key] = false
-  this.setState(dict)
-}
-}
-
-joystickStart(evt) {
-this.setJoystickProps(evt);
-}
-
-joystickMove(evt) {
-this.setJoystickProps(evt);
-this.setState({
-  touchLocation : "" + evt.nativeEvent.locationX.toFixed(2) + ", " + evt.nativeEvent.locationY.toFixed(2) + ", " + evt.nativeEvent.pageX.toFixed(2) + ", " + evt.nativeEvent.pageY.toFixed(2)
-})
-}
-
-joystickEnd(evt) {
-this.setState({
-  left : false,
-  right : false,
-  leftRightRatio : 0,
-})
-}
-
-setJoystickProps(evt) {
-let locationX = evt.nativeEvent.locationX; // position relative to top left of view
-let pageX = evt.nativeEvent.pageX; // position relative to top left of screen
-let leftValue = false;
-let rightValue = false;
-let ratio = 0;
-let halfWidth = joystickWidth / 2;
-if (Platform.OS == 'android') {
-  // on Android, the locationX reverts to pageX when off the button/view
-  if (locationX != pageX) {
-    if (locationX <= halfWidth) {
-      leftValue = true;
-      ratio = (halfWidth - locationX) / halfWidth
-    } else {
-      rightValue = true;
-      ratio = (halfWidth - joystickWidth + locationX) / halfWidth
-    }
-  } else {
-    /*
-    if we went off the button to the left or right, then we simply set the
-    ratio to max (1)
-    */
-    // TODO: this isn't actually accurate, we need to keep track of where
-    // relative to the top left corner of the button because a soon as we
-    // leave the button, we get all page coordinates. keep track on "down"
-    ratio = 1;
-    leftValue = this.state.left;
-    rightValue = this.state.right;
-  }
-} else { // iOS
-  if (locationX <= 0 || locationX >= joystickWidth) {
-    ratio = 1;
-    leftValue = this.state.left;
-    rightValue = this.state.right;
-  } else {
-    if (locationX <= halfWidth) {
-      leftValue = true;
-      ratio = (halfWidth - locationX) / halfWidth
-    } else {
-      rightValue = true;
-      ratio = (halfWidth - joystickWidth + locationX) / halfWidth
-    }
-  }
-}
-
-this.setState({
-  left : leftValue,
-  right : rightValue,
-  leftRightRatio : Math.max(Math.min(ratio, 1), 0), // bound ratio to 0 -> 1
-})
-}
-
-getReadyUI() {
-
-if (this.state.showInstructions) {
-
-  let text = this.state.isOverPlane ? ' ' : 'Finding the floor...'
-
-  let overlayStyle = {
-    position : 'absolute',
-    width : '100%',
-    height : '100%',
+    return (
+      <ViroARSceneNavigator
+        ref={(ref) => { this.arNavigator = ref }}
+        viroAppProps={viroAppProps}
+        initialScene={{
+          scene: ARScene,
+          passProps: {
+            onARInitialized: this.onARInitialized,
+            onPosterFound: this.onPosterFound,
+            onExperienceFinished: this.onExperienceFinished,
+            onARSceneCreated: this.onARSceneCreated,
+          },
+        }} />
+    );
   }
 
-  let readyButton = {
-    height: 60,
-    width: 130,
-    marginTop : 10,
-    backgroundColor:'#292930B3',
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: '#fff',
-    justifyContent : 'center',
-    alignItems : 'center',
-    opacity : this.state.isOverPlane ? 1 : .5,
+  getCarControls() {
+
+    return (
+      <Animated.View style={{
+        position: 'absolute', width: '100%',
+        height: '100%', opacity: this.state.carControlsOpacity
+      }}>
+        {/* These are the controls to drive the car */}
+        {this.getDrivingPedals()}
+        {this.getResetButton()}
+        {this.getJoystick()}
+      </Animated.View>
+    )
   }
 
-  return (
-    <Animated.View style={{...overlayStyle, opacity : this.state.instructionOpacity}}>
-      <View style={styles.readyContainer}>
-        <Text style={styles.instructionText}>
-          {text}
-        </Text>
-        <Pressable
-          style={readyButton}
-          opacity={.5}
-          onPress={this.ready}
-          disabled={!this.state.isOverPlane}
-          activeOpacity={0.6}>
-          <Text style={styles.instructionText}>
-            Place
-          </Text>
-        </Pressable>
+  getDrivingPedals() {
+    return (
+      <View style={styles.drivingButtonsContainer} >
+
+        <View style={styles.drivingButton} >
+          <Image style={styles.pedalImage} opacity={this.state.down ? 0 : 1}
+            source={require('../assets/pedal_reverse.png')} />
+          <Image style={styles.pedalImage} opacity={!this.state.down ? 0 : 1}
+            source={require('../assets/pedal_reverse_press.png')} />
+          <View style={styles.pedalTouchArea} onTouchStart={this.getPressDown('down')}
+            onTouchEnd={this.getPressUp('down')} />
+        </View>
+
+        <View style={styles.drivingButton} >
+          <Image style={styles.pedalImage} opacity={this.state.up ? 0 : 1}
+            source={require('../assets/pedal_accel.png')} />
+          <Image style={styles.pedalImage} opacity={!this.state.up ? 0 : 1}
+            source={require('../assets/pedal_accel_press.png')} />
+          <View style={styles.pedalTouchArea} onTouchStart={this.getPressDown('up')}
+            onTouchEnd={this.getPressUp('up')} />
+        </View>
+
       </View>
-    </Animated.View>
-  )
-}
-}
-
-ready() {
-  // only allow ready to be clicked when the user has click over a plane!
-  if (!this.state.isOverPlane) {
-    return;
+    )
   }
 
-  Animated.timing(this.state.instructionOpacity, {
-    toValue : 0,
-    duration : 1000,
-    useNativeDriver: true,
-    easing : Easing.linear,
-  }).start(()=>{
-    this.setState({
-      showInstructions : false,
-      isReady : true,
-    })
-  })
+  getJoystick() {
+    let rotation = '' + (this.state.left ? '-' : '') + Math.round(this.state.leftRightRatio * 90) + 'deg'
 
-  setTimeout(()=>{
-    Animated.timing(this.state.carControlsOpacity, {
-      toValue : 1,
-      duration : 500,
+    let joystickButtonStyle = {
+      height: 130,
+      width: 200,
+      resizeMode: 'contain',
+      transform: [{ rotate: rotation }]
+    }
+
+    /*
+      This is the joystick/steering wheel component, since the image is rotating, we need
+      that "invisible" view to capture the touch events.
+     */
+    return (
+      <View style={styles.joystickContainer} >
+        <Image style={joystickButtonStyle} source={require('../assets/steering_wheel.png')} />
+        <View style={styles.joystickTouchArea} onTouchStart={this.joystickStart}
+          onTouchMove={this.joystickMove} onTouchEnd={this.joystickEnd} />
+      </View>
+    )
+  }
+
+  getResetButton() {
+    return (
+      <Pressable
+        style={styles.resetButton}
+        onPress={this.resetCar}
+        activeOpacity={0.6} >
+        <Image style={styles.resetImage} source={require('../assets/icon_refresh.png')} />
+      </Pressable>
+    )
+  }
+
+  resetCar() {
+    this.setState({
+      shouldResetCar: true,
+    })
+
+    // reset the flag 1 second later.
+    setTimeout(() => {
+      this.setState({
+        shouldResetCar: false,
+      })
+    }, 1000)
+  }
+
+  getPressDown(key) {
+    return () => {
+      let dict = {}
+      dict[key] = true
+      this.setState(dict)
+    }
+  }
+
+  getPressUp(key) {
+    return () => {
+      let dict = {}
+      dict[key] = false
+      this.setState(dict)
+    }
+  }
+
+  joystickStart(evt) {
+    this.setJoystickProps(evt);
+  }
+
+  joystickMove(evt) {
+    this.setJoystickProps(evt);
+    this.setState({
+      touchLocation: "" + evt.nativeEvent.locationX.toFixed(2) + ", " + evt.nativeEvent.locationY.toFixed(2) + ", " + evt.nativeEvent.pageX.toFixed(2) + ", " + evt.nativeEvent.pageY.toFixed(2)
+    })
+  }
+
+  joystickEnd(evt) {
+    this.setState({
+      left: false,
+      right: false,
+      leftRightRatio: 0,
+    })
+  }
+
+  setJoystickProps(evt) {
+    let locationX = evt.nativeEvent.locationX; // position relative to top left of view
+    let pageX = evt.nativeEvent.pageX; // position relative to top left of screen
+    let leftValue = false;
+    let rightValue = false;
+    let ratio = 0;
+    let halfWidth = joystickWidth / 2;
+    if (Platform.OS == 'android') {
+      // on Android, the locationX reverts to pageX when off the button/view
+      if (locationX != pageX) {
+        if (locationX <= halfWidth) {
+          leftValue = true;
+          ratio = (halfWidth - locationX) / halfWidth
+        } else {
+          rightValue = true;
+          ratio = (halfWidth - joystickWidth + locationX) / halfWidth
+        }
+      } else {
+        /*
+        if we went off the button to the left or right, then we simply set the
+        ratio to max (1)
+        */
+        // TODO: this isn't actually accurate, we need to keep track of where
+        // relative to the top left corner of the button because a soon as we
+        // leave the button, we get all page coordinates. keep track on "down"
+        ratio = 1;
+        leftValue = this.state.left;
+        rightValue = this.state.right;
+      }
+    } else { // iOS
+      if (locationX <= 0 || locationX >= joystickWidth) {
+        ratio = 1;
+        leftValue = this.state.left;
+        rightValue = this.state.right;
+      } else {
+        if (locationX <= halfWidth) {
+          leftValue = true;
+          ratio = (halfWidth - locationX) / halfWidth
+        } else {
+          rightValue = true;
+          ratio = (halfWidth - joystickWidth + locationX) / halfWidth
+        }
+      }
+    }
+
+    this.setState({
+      left: leftValue,
+      right: rightValue,
+      leftRightRatio: Math.max(Math.min(ratio, 1), 0), // bound ratio to 0 -> 1
+    })
+  }
+
+  getReadyUI() {
+
+    if (this.state.showInstructions) {
+
+      let text = this.state.isOverPlane ? ' ' : 'Finding the floor...'
+
+      let overlayStyle = {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+      }
+
+      let readyButton = {
+        height: 60,
+        width: 130,
+        marginTop: 10,
+        backgroundColor: '#292930B3',
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        opacity: this.state.isOverPlane ? 1 : .5,
+      }
+
+      return (
+        <Animated.View style={{ ...overlayStyle, opacity: this.state.instructionOpacity }}>
+          <View style={styles.readyContainer}>
+            <Text style={styles.instructionText}>
+              {text}
+            </Text>
+            <Pressable
+              style={readyButton}
+              opacity={.5}
+              onPress={this.ready}
+              disabled={!this.state.isOverPlane}
+              activeOpacity={0.6}>
+              <Text style={styles.instructionText}>
+                Place
+              </Text>
+            </Pressable>
+          </View>
+        </Animated.View>
+      )
+    }
+  }
+
+  ready() {
+    // only allow ready to be clicked when the user has click over a plane!
+    if (!this.state.isOverPlane) {
+      return;
+    }
+
+    Animated.timing(this.state.instructionOpacity, {
+      toValue: 0,
+      duration: 1000,
       useNativeDriver: true,
-      easing : Easing.linear,
-    }).start()
-  }, 1000)
-}
-
-getInstructions() {
-
-  if (!this.state.showInstructions) {
-    return
-  }
-
-  var instructionContainer = {
-    position : 'absolute',
-    backgroundColor : '#000000B3',
-    flexDirection : 'column',
-    width : '100%',
-    height : 100,
-    justifyContent : 'center',
-    top : 0,
-    left : 0,
-  }
-
-  let instructions = "Scan the ground and tap Place to begin."
-
-  return (
-    <Animated.View style={{...instructionContainer, opacity : this.state.instructionOpacity}}>
-      <Text style={styles.instructionText}>
-        {instructions}
-      </Text>
-    </Animated.View>
-  )
-}
-
-// this function will be called by the AR system when the user is hovering over a plane
-setIsOverPlane(isOverPlane) {
-  if (this.state.isOverPlane != isOverPlane) {
-    this.setState({
-      isOverPlane : isOverPlane,
+      easing: Easing.linear,
+    }).start(() => {
+      this.setState({
+        showInstructions: false,
+        isReady: true,
+      })
     })
-  }
-}
 
-exitAR() {
-  this.props.navigation.goBack();
-}
+    setTimeout(() => {
+      Animated.timing(this.state.carControlsOpacity, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+        easing: Easing.linear,
+      }).start()
+    }, 1000)
+  }
+
+  getInstructions() {
+
+    if (!this.state.showInstructions) {
+      return
+    }
+
+    var instructionContainer = {
+      position: 'absolute',
+      backgroundColor: '#000000B3',
+      flexDirection: 'column',
+      width: '100%',
+      height: 100,
+      justifyContent: 'center',
+      top: 0,
+      left: 0,
+    }
+
+    let instructions = "Scan the ground and tap Place to begin."
+
+    return (
+      <Animated.View style={{ ...instructionContainer, opacity: this.state.instructionOpacity }}>
+        <Text style={styles.instructionText}>
+          {instructions}
+        </Text>
+      </Animated.View>
+    )
+  }
+
+  // this function will be called by the AR system when the user is hovering over a plane
+  setIsOverPlane(isOverPlane) {
+    if (this.state.isOverPlane != isOverPlane) {
+      this.setState({
+        isOverPlane: isOverPlane,
+      })
+    }
+  }
+
+  exitAR() {
+    this.props.navigation.goBack();
+  }
 
 }
 
