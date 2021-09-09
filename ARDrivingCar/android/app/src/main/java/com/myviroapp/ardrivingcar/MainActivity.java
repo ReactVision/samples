e3db3e9bcd801ba7504429d9f9cade967d2e4fa5
Kurt VanDusen <info@equationalapplications.com>
2021-09-09 19:15:15 -0400

    npx react-native-rename ardrivingcar -b com.myviroapp.ardrivingcar

--- ARDrivingCar/android/app/src/main/java/com/myviroapp/ardrivingcar/MainActivity.java
+++ ARDrivingCar/android/app/src/main/java/com/myviroapp/ardrivingcar/MainActivity.java
@@ -1,4 +1,4 @@
-package com.equationalapplications.arbot;
+package com.myviroapp.ardrivingcar;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
@@ -11,7 +11,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "arbot";
+    return "ardrivingcar";
   }
   @Override
   protected void onCreate(Bundle savedInstanceState) {
--- ARDrivingCar/android/app/src/main/java/com/myviroapp/ardrivingcar/MainApplication.java
+++ ARDrivingCar/android/app/src/main/java/com/myviroapp/ardrivingcar/MainApplication.java
@@ -1,4 +1,4 @@
-package com.equationalapplications.arbot;
+package com.myviroapp.ardrivingcar;
 
 import android.app.Application;
 import android.content.Context;
@@ -64,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.equationalapplications.arbot.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.myviroapp.ardrivingcar.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
